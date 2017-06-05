c6865a876e59e96c3885c3aed04eb0ce5a25efaa
Chasteau <davidchasteau@gmail.com>
2017-06-05 10:33:39 +0000

    second commit

--- https.js
+++ https.js
@@ -13,8 +13,23 @@ https.request(options, callback).end();
 
 console.log("I've made the request!");
 
+var options = {
+  host: 'stream-large-file.herokuapp.com',
+  path: '/give-me-stuff-now'
+};
+
 // called by https when the request is made.
 var callback = function(response) {
   console.log('In response handler callback!');
   console.log('Response: ', response);
+}
+
+// called by https when the request is made.
+var callback = function(response) {
+  console.log('In response handler callback!');
+
+  response.on('data', function(chunk) {
+    console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
+    console.log(chunk.toString());
+  });
 }
